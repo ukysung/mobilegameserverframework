cc49092ca773146d894d279dbb208bfa0ea69259
MobileGameServer <mobilegameserver@gmail.com>
2016-02-23 12:24:11 +0900

    trivial

--- gen/protoc.bat
+++ gen/protoc.bat
@@ -1,11 +1,11 @@
 
-protoc.exe --python_out=..\bin msg_type_data.proto
-protoc.exe --python_out=..\bin msg_type_play.proto
-protoc.exe --python_out=..\bin msg_enum.proto
-protoc.exe --python_out=..\bin msg_struct.proto
-protoc.exe --python_out=..\bin msg_error.proto
-protoc.exe --python_out=..\bin msg_packet_data.proto
-protoc.exe --python_out=..\bin msg_packet_play.proto
+protoc.exe --python_out=..\src msg_type_data.proto
+protoc.exe --python_out=..\src msg_type_play.proto
+protoc.exe --python_out=..\src msg_enum.proto
+protoc.exe --python_out=..\src msg_struct.proto
+protoc.exe --python_out=..\src msg_error.proto
+protoc.exe --python_out=..\src msg_packet_data.proto
+protoc.exe --python_out=..\src msg_packet_play.proto
 
 ProtoGen.exe --include_imports msg_type_data.proto
 ProtoGen.exe --include_imports msg_type_play.proto
