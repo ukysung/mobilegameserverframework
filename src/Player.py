0d2d776e55a36f433b1857b0ac9c965edabb7963
MobileGameServer <mobilegameserver@gmail.com>
2016-02-26 14:10:21 +0900

    trivial

--- src/Player.py
+++ src/Player.py
@@ -8,14 +8,14 @@ from PlayerAbnormality import PlayerAbnormality
 class Player:
     def __init__(self, area_id):
         self.area_id = area_id
-        #self.attr = PlayerAttribute(self)
+        self.attr = PlayerAttribute(self)
         self.move = PlayerMovement(self)
         self.skill = PlayerSkill(self)
         self.action = PlayerAction(self)
         self.abnormality = PlayerAbnormality(self)
 
     def run(self, delta_time):
-        #self.attr.run(delta_time)
+        self.attr.run(delta_time)
         self.move.run(delta_time)
         self.skill.run(delta_time)
         self.action.run(delta_time)
