c82e2a9c21381d7a23478fac0c193921c0863f0a
MobileGameServer <mobilegameserver@gmail.com>
2016-03-02 12:38:02 +0900

    play -> channel

--- msg/protoc.bat
+++ msg/protoc.bat
@@ -1,18 +1,18 @@
 
 protoc.exe --python_out=..\src msg_type_data.proto
-protoc.exe --python_out=..\src msg_type_play.proto
+protoc.exe --python_out=..\src msg_type_channel.proto
 protoc.exe --python_out=..\src msg_enum.proto
 protoc.exe --python_out=..\src msg_struct.proto
 protoc.exe --python_out=..\src msg_error.proto
 protoc.exe --python_out=..\src msg_packet_data.proto
-protoc.exe --python_out=..\src msg_packet_play.proto
+protoc.exe --python_out=..\src msg_packet_channel.proto
 
 ProtoGen.exe --include_imports msg_type_data.proto
-ProtoGen.exe --include_imports msg_type_play.proto
+ProtoGen.exe --include_imports msg_type_channel.proto
 ProtoGen.exe --include_imports msg_enum.proto
 ProtoGen.exe --include_imports msg_struct.proto
 ProtoGen.exe --include_imports msg_error.proto
 ProtoGen.exe --include_imports msg_packet_data.proto
-ProtoGen.exe --include_imports msg_packet_play.proto
+ProtoGen.exe --include_imports msg_packet_channel.proto
 
 pause
--- msg/protoc.sh
+++ msg/protoc.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 protoc --python_out=../src msg_type_data.proto
-protoc --python_out=../src msg_type_play.proto
+protoc --python_out=../src msg_type_channel.proto
 protoc --python_out=../src msg_enum.proto
 protoc --python_out=../src msg_struct.proto
 protoc --python_out=../src msg_error.proto
 protoc --python_out=../src msg_packet_data.proto
-protoc --python_out=../src msg_packet_play.proto
+protoc --python_out=../src msg_packet_channel.proto
 
