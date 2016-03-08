e3b08974d23286ecf79ef2a224b3262f08ea5040
MobileGameServer <mobilegameserver@gmail.com>
2016-03-08 13:16:23 +0900

    msg.py added

---
+++ src/msg.py
@@ -0,0 +1,20 @@
+
+import struct
+
+'''
+struct
+{
+	int32_t msg_type; // 4 bytes
+	int32_t msg_size; // 4 bytes
+}
+// totally 8 bytes
+'''
+header_size = 8
+
+def unpack_head(msg_head):
+    return struct.unpack('ii', msg_head)
+
+def pack(msg_type, msg):
+    msg_str = msg.SerializeToString()
+    return struct.pack('ii', msg_type, len(msg_str)) + msg_str
+
