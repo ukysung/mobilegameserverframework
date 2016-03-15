7ed8e9e75cbae0f18dbeb7659e964685b9363933
MobileGameServer <mobilegameserver@gmail.com>
2016-03-15 12:58:11 +0900

    PROC_POOL Test

--- src/data_handle_sign_up.py
+++ src/data_handle_sign_up.py
@@ -10,6 +10,9 @@ import msg_struct_pb2
 import msg_error_pb2
 import msg_packet_data_pb2
 
+def b(i):
+    return i
+
 @asyncio.coroutine
 def handle_sign_up(req_msg_type, req_msg_body):
     req = msg_packet_data_pb2.sign_up_req()
@@ -17,9 +20,13 @@ def handle_sign_up(req_msg_type, req_msg_body):
 
     ack = msg_packet_data_pb2.sign_up_ack()
     ack.err_code = msg_error_pb2.err_server_unknown
-    ack.auth_token = str(g.MST[1])
+    ack.auth_token = ''
 
     # do something with db here
+    j = yield from asyncio.futures.wrap_future(g.PROC_POOL.submit(b, 1))
+
+    ack.err_code = msg_error_pb2.err_none
+    ack.auth_token = str(g.MST[1] + j)
 
     return msg.pack(msg_type_data_pb2.t_sign_up_ack, ack)
 g.DATA_HANDLERS[msg_type_data_pb2.t_sign_up_req] = handle_sign_up
